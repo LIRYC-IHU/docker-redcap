1565355d3e3151b7903b6611d2e6ee732d5e3a85
Josszzz <josselin.duchateau@gmail.com>
2026-06-03 20:56:32 +0200

    Add wasm build

---
+++ girder_uploader_v1.1.0/wasm/dicom_deid/pkg/dicom_deid.d.ts
@@ -0,0 +1,54 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export class DeidentifyResult {
+    private constructor();
+    free(): void;
+    [Symbol.dispose](): void;
+    bytes(): Uint8Array;
+    formatName(): string;
+    mimeType(): string;
+}
+
+export function deidentify(input_bytes: Uint8Array, file_name: string, record_id: string, patient_name: string, enable_dicom: boolean, enable_xml: boolean, enable_schiller: boolean): DeidentifyResult;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly __wbg_deidentifyresult_free: (a: number, b: number) => void;
+    readonly deidentify: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number) => [number, number, number];
+    readonly deidentifyresult_bytes: (a: number) => [number, number];
+    readonly deidentifyresult_formatName: (a: number) => [number, number];
+    readonly deidentifyresult_mimeType: (a: number) => [number, number];
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
